47756157b53b4915406a5000c11396707505393c
Theoden Melgar <theodenmelgar@gmail.com>
2025-10-04 18:30:21 -0700

    added jwt auth to backend, with register and login api routes. additional config is necessary for deciding what routes should be public or private

---
+++ backend/src/main/java/com/backend/user/AuthenticationService.java
@@ -0,0 +1,48 @@
+package com.backend.user;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationService(
+            UserRepository userRepository,
+            AuthenticationManager authenticationManager,
+            PasswordEncoder passwordEncoder
+    ) {
+        this.authenticationManager = authenticationManager;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User signup(RegisterUserDTO input) {
+        User user = new User();
+
+        user.setUsername(input.getUsername());
+        user.setEmail(input.getEmail());
+        user.setPassword(passwordEncoder.encode(input.getPassword()));
+
+        System.out.println(user);
+        return userRepository.save(user);
+    }
+
+    public User authenticate(LoginUserDTO input) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        input.getUsername(),
+                        input.getPassword()
+                )
+        );
+
+        return userRepository.findByUsername(input.getUsername())
+                .orElseThrow();
+    }
+}
